028521f852d40bb36c228fa4b6e783aab20a314b
LeeJY97 <ijoonyeol97@gmail.com>
2024-10-19 20:43:17 +0900

    .

--- 빈도수 세기/01_아나그램/anagrams.js
+++ 빈도수 세기/01_아나그램/anagrams.js
@@ -7,7 +7,22 @@ function validAnagram(standardStr, compareStr) {
     standardCount[c] = !standardCount[c] ? 1 : standardCount[c] + 1;
   }
 
-  console.log('standardCount', standardCount)
+  for (const c of compareStr) {
+    if (!standardCount[c]) return false;
+    standardCount[c] -= 1;
+  }
+
+  return true;
+}
+
+function validAnagram(standardStr, compareStr) {
+  const standardCount = {};
+
+  if (standardStr.length !== compareStr.length) return false;
+
+  for (const c of standardStr) {
+    standardCount[c] = !standardCount[c] ? 1 : standardCount[c] + 1;
+  }
 
   for (const c of compareStr) {
     if (!standardCount[c]) return false;
@@ -17,11 +32,20 @@ function validAnagram(standardStr, compareStr) {
   return true;
 }
 
-// validAnagram('', '') // true
-// validAnagram('anagram', 'nagaram') // true
-// validAnagram('qwerty', 'qeywrt') // true
-// validAnagram('texttwisttime', 'timetwisttext') // true
-// validAnagram('aaz', 'zza') // false
-// validAnagram("rat", "car")  // false
-// validAnagram('awesome', 'awesom') // false
-// validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+const a = validAnagram('', '') // true
+const b = validAnagram('anagram', 'nagaram') // true
+const c = validAnagram('qwerty', 'qeywrt') // true
+const d = validAnagram('texttwisttime', 'timetwisttext') // true
+const e = validAnagram('aaz', 'zza') // false
+const f = validAnagram("rat", "car")  // false
+const g = validAnagram('awesome', 'awesom') // false
+const h = validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+
+console.log('a', a)
+console.log('b', b)
+console.log('c', c)
+console.log('d', d)
+console.log('e', e)
+console.log('f', f)
+console.log('g', g)
+console.log('h', h)
