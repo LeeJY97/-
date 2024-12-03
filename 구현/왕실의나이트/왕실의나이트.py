1624d4d4feeab08df383ae41b0f90e6e4df527ae
LeeJY97 <ijoonyeol97@gmail.com>
2024-12-03 17:15:51 +0900

    [구현] - 왕실의나이트

---
+++ 구현/왕실의나이트/왕실의나이트.py
@@ -0,0 +1,17 @@
+def solution(position):
+
+  row = int(position[1])
+  column = int(ord(position[0])) - int(ord('a')) + 1
+
+  steps = [(-2, -1), (-1, -2), (1, -2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
+
+  result = 0
+  for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+    if(next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8):
+      result+=1
+
+  return result
+
+solution("a1");
